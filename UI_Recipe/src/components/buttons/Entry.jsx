a936fab4785cd597b0e285539d9e7e4fd0403129
Ranjel272 <162986972+Ranjel272@users.noreply.github.com>
2025-06-24 13:00:52 +0800

    ENtry

--- UI_Recipe/src/components/buttons/Entry.jsx
+++ UI_Recipe/src/components/buttons/Entry.jsx
@@ -2,7 +2,7 @@ import * as React from "react";
 import { Slot } from "@radix-ui/react-slot";
 import PropTypes from "prop-types";
 import clsx from "clsx"; // Ensure clsx is installed using npm install clsx
-import "./entry.css"; // Ensure you have the corresponding CSS file
+import "./Entry.css"; // Ensure you have the corresponding CSS file
 
 const Entry = React.forwardRef(({ className, asChild = false, ...props }, ref) => {
   const Comp = asChild ? Slot : "button";

cfdbe63d641340dc3cb6599c65b504ac16a05e31
Ranjel272 <162986972+Ranjel272@users.noreply.github.com>
2025-06-24 12:53:22 +0800

    Cards

--- UI_Recipe/src/components/Landing_Page/RecipePreview.jsx
+++ UI_Recipe/src/components/Landing_Page/RecipePreview.jsx
@@ -6,7 +6,7 @@ import {
   CardFooter,
   CardHeader,
   CardTitle,
-} from "../buttons/card";
+} from "../buttons/Card";
 import { Button } from "../buttons/button";
 import { Clock, Users, Utensils } from "lucide-react";
 import "./RecipePreview.css";
